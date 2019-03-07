6aa4a4ced30263dd61477c574d9b0231418ed35b
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-03-07 18:11:07 +0100

    Implement TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -2,11 +2,19 @@ package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        if (value == null) {
+            return new ArrayList<>();
+        }
+
+        int characters = value.length();
+        return IntStream.rangeClosed(0, characters).mapToObj(x -> value.substring(x, characters)).collect(Collectors.toList());
     }
 
 }
+
